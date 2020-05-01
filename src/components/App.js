b8cfddfd0ad10024e18537b26961a432e9b3730d
Francisross Soares de Oliveira <franciscpd@gmail.com>
2020-05-01 19:31:49 -0300

    feat: initial setup boilerplate

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import { hot } from 'react-hot-loader/root';
+import React from 'react';
+
+const App = () => {
+  // const {
+  //   components: { Welcome },
+  // } = welcome;
+
+  return (
+    <div className="App">
+      <h1>Welcome</h1>
+    </div>
+  );
+};
+
+export default hot(App);
---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from '@hot-loader/react-dom';
+import App from '@components/App';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
